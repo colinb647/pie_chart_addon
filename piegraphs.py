4b95d3624302e4f1b2cde9b378177aa8908577e7
Colin <colinb647@gmail.com>
2019-03-13 09:23:32 -0400

    formatting edits to README.md and piegraphs to removed unused

--- piegraphs.py
+++ piegraphs.py
@@ -2,10 +2,6 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import time
-import datetime
-import json
-import math
 import anki.stats
 import anki.collection
 from anki.hooks import wrap
