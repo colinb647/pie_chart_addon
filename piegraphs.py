b2ffa8c6d7d5038e69334e88c76760e27ad4a873
Colin <colinb647@gmail.com>
2019-03-16 15:24:28 -0400

    new beta branch

--- piegraphs.py
+++ piegraphs.py
@@ -52,11 +52,7 @@ def cardGraph_alt(self):
         info)
     return txt
 
-anki.stats.CollectionStats.cardGraph = wrap(anki.stats.CollectionStats.cardGraph, cardGraph_alt, pos="after")
-
-###############################################################################
-
-def _cards_alt(self, _old):
+def _cards_lrn(self, _old):
     return self.col.db.first("""
         select
         sum(case when queue=2 and ivl >= 21 then 1 else 0 end), -- mtr
@@ -66,4 +62,98 @@ def _cards_alt(self, _old):
         sum(case when queue<0 then 1 else 0 end) -- susp
         from cards where did in %s""" % self._limit())
 
-anki.stats.CollectionStats._cards = wrap(anki.stats.CollectionStats._cards, _cards_alt, pos="around")
+############################################################################
+
+def cardGraph_relrn(self, _old):
+    # pie graph data
+    div = self._cards()
+    d = []
+    for c, (t, col) in enumerate((
+        (_("Mature"), colMature),
+        (_("Young"), colYoung),
+        (_("Relearn"), colRelearn),
+        (_("Unseen"), colUnseen),
+        (_("Suspended+Buried"), colSusp))):
+        d.append(dict(data=div[c], label="%s: %s" % (t, div[c]), color=col))
+    # text data
+    i = []
+    (c, f) = self.col.db.first("""
+        select count(id), count(distinct nid) from cards
+        where did in %s """ % self._limit())
+    self._line(i, _("Total cards"), c)
+    self._line(i, _("Total notes"), f)
+    (low, avg, high) = self._factors()
+    if low:
+        self._line(i, _("Lowest ease"), "%d%%" % low)
+        self._line(i, _("Average ease"), "%d%%" % avg)
+        self._line(i, _("Highest ease"), "%d%%" % high)
+    info = "<table width=100%>" + "".join(i) + "</table><p>"
+    info += _('''\
+        A card's <i>ease</i> is the size of the next interval \
+        when you answer "good" on a review.''')
+    txt = self._title(_("Card Types"),
+                      _("The division of cards in your deck(s)."))
+    txt += "<table width=%d><tr><td>%s</td><td>%s</td></table>" % (
+        self.width,
+        self._graph(id="cards", data=d, type="pie"),
+        info)
+    return txt
+
+def _cards_relrn(self, _old):
+    return self.col.db.first("""
+        select
+        sum(case when queue=2 and ivl >= 21 then 1 else 0 end), -- mtr
+        sum(case when queue=2 and ivl < 21 then 1 else 0 end), -- yng
+        sum(case when queue in (1,3) then 1 else 0 end), --relrn
+        sum(case when queue=0 then 1 else 0 end), -- new
+        sum(case when queue<0 then 1 else 0 end) -- susp
+        from cards where did in %s""" % self._limit())
+
+############################################################################
+
+def cardGraph_lrn_relrn(self, _old):
+    # pie graph data
+    div = self._cards()
+    d = []
+    for c, (t, col) in enumerate((
+        (_("Mature"), colMature),
+        (_("Young"), colYoung),
+        (_("Learn"), colLearn),
+        (_("Relearn"), colRelearn)
+        (_("Unseen"), colUnseen),
+        (_("Suspended+Buried"), colSusp))):
+        d.append(dict(data=div[c], label="%s: %s" % (t, div[c]), color=col))
+    # text data
+    i = []
+    (c, f) = self.col.db.first("""
+        select count(id), count(distinct nid) from cards
+        where did in %s """ % self._limit())
+    self._line(i, _("Total cards"), c)
+    self._line(i, _("Total notes"), f)
+    (low, avg, high) = self._factors()
+    if low:
+        self._line(i, _("Lowest ease"), "%d%%" % low)
+        self._line(i, _("Average ease"), "%d%%" % avg)
+        self._line(i, _("Highest ease"), "%d%%" % high)
+    info = "<table width=100%>" + "".join(i) + "</table><p>"
+    info += _('''\
+        A card's <i>ease</i> is the size of the next interval \
+        when you answer "good" on a review.''')
+    txt = self._title(_("Card Types"),
+                      _("The division of cards in your deck(s)."))
+    txt += "<table width=%d><tr><td>%s</td><td>%s</td></table>" % (
+        self.width,
+        self._graph(id="cards", data=d, type="pie"),
+        info)
+    return txt
+
+def _cards_lrn_relrn(self, _old):
+    return self.col.db.first("""
+        select
+        sum(case when queue=2 and ivl >= 21 then 1 else 0 end), -- mtr
+        sum(case when queue=2 and ivl < 21 then 1 else 0 end), -- yng
+        sum(case when queue in (1,3) then 1 else 0 end), --lrn
+        sum(), --relrn
+        sum(case when queue=0 then 1 else 0 end), -- new
+        sum(case when queue<0 then 1 else 0 end) -- susp
+        from cards where did in %s""" % self._limit())
